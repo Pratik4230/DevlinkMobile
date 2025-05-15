52a97c0a102227c3215c23cd3ab6ed60ff6d5a63
Pratik4230 <pratikjadhav1438@gmail.com>
2025-05-15 23:31:37 +0530

    feed in progress

--- app/(tabs)/feed.tsx
+++ app/(tabs)/feed.tsx
@@ -1,14 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { api } from '@/utils/api';
+import ThemedText from '@/components/UI/ThemedText';
+import ThemedView from '@/components/UI/ThemedView';
 
 const feed = () => {
+
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(15);
+
+
+  const {data:feed , isLoading, isError} = useQuery({
+    queryKey: ["feed", limit, page],
+    queryFn: async () => {
+      const response = await api.get(`/user/feed?page=${page}&limit=${limit}`);
+      return response.data
+    }
+  })
+
+  const renderFeed = ({item}: any) => {
+    return (
+      <ThemedView>
+<ThemedText > Helllllllllllo {item._id} </ThemedText>
+      </ThemedView>
+    ) 
+  }
+
+  if (isLoading) {
+    return <ActivityIndicator size="large" />
+  }
+
+  console.log("feed : ", feed);
+  
+
   return (
-    <View>
-      <Text>feed</Text>
-    </View>
+    <FlatList 
+    data={feed.data}
+    renderItem={renderFeed}
+    keyExtractor={(item) => item._id}
+    showsVerticalScrollIndicator={false}
+    contentContainerStyle={styles.postlist}
+    
+
+    />
   )
 }
 
 export default feed
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  postlist: {
+    backgroundColor: "black"
+  }
+})

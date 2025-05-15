90dcac3728d3f3122baf5d2bac53ddad12bb3583
Pratik4230 <pratikjadhav1438@gmail.com>
2025-05-15 23:31:27 +0530

    tab layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet} from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { Colors } from '@/constants/Colors';
+
+import {Ionicons} from "@expo/vector-icons";
 
 const  TabsLayout  = () => {
   return (
-    <Tabs>
-        <Tabs.Screen name='feed' options={{}} />
-                <Tabs.Screen name='connection' options={{}} />
-                        <Tabs.Screen name='jobs' options={{}} />
-                                <Tabs.Screen name='chat' options={{}} />
+    <Tabs
+    screenOptions={{
+      headerShown: false,
+      tabBarActiveTintColor: Colors.blue400,
+      tabBarInactiveTintColor: "#888",
+      tabBarStyle: {
+        backgroundColor: "black",
+        borderTopColor: "blue"
+      },
+
+      
+    }}
+    >
+
+        <Tabs.Screen name='feed' options={{
+          title: "Feed",
+          tabBarIcon: ({color, size, focused}) => (
+            <Ionicons
+            name={focused ? "home" : "home-outline"}
+            size={size}
+            color={color}
+            />
+          ),
+
+        }} />
+
+              <Tabs.Screen
+        name="connection"
+        options={{
+          title: 'Connections',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? 'people' : 'people-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+ <Tabs.Screen
+        name="jobs"
+        options={{
+          title: 'Jobs',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? 'briefcase' : 'briefcase-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+       <Tabs.Screen
+        name="chat"
+        options={{
+          title: 'Chat',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? 'chatbox-ellipses' : 'chatbox-ellipses-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+
     </Tabs>
   )
 }
